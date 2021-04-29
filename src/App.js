5771ea84b63523360fe7ffc27049b7ba32c4edc2
Luis Angel <luisangelpenacemas@gmail.com>
2021-04-28 14:54:10 -1200

    Modificando index

--- src/App.js
+++ src/App.js
@@ -1,35 +1,3 @@
-<<<<<<< HEAD
-
-
-
-
-
-import Button from './components/Button';
-import {signInWithGoogle, signOut } from './login';
-
-function App () {
-
-  return(
-     <div>
-           {
-
-              user ? (
-
-                    <>
-                         <button onClick={signOut}> Sign in with Google</button>
-                         <p>Bienbenidos al CHAT! </p>
-                         
-                    </>
-
-              ) : <button onClick={signInWithGoogle}>Sign in with Google </button>
-
-           }
-
-
-     </div>
-
-
-=======
 import {
   db,
   googleAuthProvider,
@@ -56,7 +24,6 @@ function App() {
     <div> 
     <Button onClick={signInWithGoogle}> Sign in with Google </Button>
     </div>
->>>>>>> 60ee6f6dbb619ffb52e5f3c843e7970917221f36
   );
 
 }
